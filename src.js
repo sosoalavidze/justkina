4e7ea1de91678e199e1b1555723fbde67df66554
sosoalavidze <ioalavidze@vassar.edu>
2020-05-28 15:10:02 -0400

    Update src.js

--- src.js
+++ src.js
@@ -1,1 +1,29 @@
-function visitPage(){window.open("https://www.youtube.com/watch?v=rL6RRIOZyCM")}function validateSeed(){for(var e=document.getElementById("myText").value,t="",n=(e=e.slice(2)).length,a=0;a<n;){var l=e.slice(a,a+=2);t+=String.fromCharCode(parseInt(l,16))}if(t.match(/^(?=.*\d)(?=.*[a-zA-Z]).{7,7}$/)){document.getElementById("result_field").innerHTML="Congratualtions, you have passed level 2. Please click the button below for your last clue.";var o=document.getElementsByClassName("main-container")[0],i=document.createElement("button");i.innerHTML="Next puzzle",i.setAttribute("class","button_next"),i.setAttribute("onclick","visitPage();"),o.appendChild(i),document.getElementsByClassName("button_validate")[0].remove()}else document.getElementById("result_field").innerHTML="Sorry, that is not a valid seed."}
+function visitPage() {
+  window.open('https://www.youtube.com/watch?v=rL6RRIOZyCM');
+}
+function validateSeed() {
+  var seed = document.getElementById("myText").value;
+  var seed_decoded="";
+  seed = seed.slice(2);
+  var length = seed.length;
+  for (var i = 0; i < length;) {
+    var code = seed.slice(i, i += 2);
+    seed_decoded += String.fromCharCode(parseInt(code, 16));
+  }
+
+  var RegEx = /^(?=.*\d)(?=.*[a-zA-Z]).{7,7}$/;
+  if(seed_decoded.match(RegEx)){
+    document.getElementById("result_field").innerHTML = "Congratualtions, you have passed level 2. Please click the button below for your last clue.";
+    var parent = document.getElementsByClassName("main-container")[0];
+    var btn_nxt = document.createElement("button");
+    btn_nxt.innerHTML = "Next puzzle";
+    btn_nxt.setAttribute("class", "button_next");
+    btn_nxt.setAttribute("onclick", "visitPage();");
+    parent.appendChild(btn_nxt);
+
+    var btn_old = document.getElementsByClassName("button_validate");
+    btn_old[0].remove();
+  }else{
+    document.getElementById("result_field").innerHTML = "Sorry, that is not a valid seed.";
+}
+}
